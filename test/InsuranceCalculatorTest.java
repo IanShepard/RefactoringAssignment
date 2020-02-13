e076bbac35540ed3e97b0a683f09ec1874e16784
Ian Shepard <ianshepard@gmail.com>
2020-02-13 11:41:28 -0500

    Create and running test

--- test/InsuranceCalculatorTest.java
+++ test/InsuranceCalculatorTest.java
@@ -4,4 +4,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InsuranceCalculatorTest {
 
+    public void setup() {
+        InsuranceCalculator calculator = new InsuranceCalculator();
+    }
+
+    public void teardown() {
+    }
+
+    @Test
+    public void extremeHighIncome() {
+        double income = 1000000;
+        assertEquals((income-60000)*0.02+105600, new InsuranceCalculator().calculateInsurance(income));
+    }
+
+    @Test
+    public void highIncome() {
+        double income = 60000;
+        assertEquals((income-30000)*0.1+76500, new InsuranceCalculator().calculateInsurance(income));
+    }
+
+    @Test
+    public void mediumIncome() {
+        double income = 30000;
+        assertEquals((income-10000)*0.2+35600 , new InsuranceCalculator().calculateInsurance(income));
+    }
+
+    @Test
+    public void lowIncome() {
+        double income = 10000;
+        assertEquals(income*0.365 , new InsuranceCalculator().calculateInsurance(income));
+    }
+
 }
